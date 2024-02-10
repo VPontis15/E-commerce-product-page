5867738a387a4f3d95d9c2870d934b23e8e60712
Vassilis Pontis <vpontis15@gmail.com>
2024-02-10 19:59:45 +0200

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'heading-clr': 'hsl(220, 13%, 13%)',
+      'text-clr': 'hsl(220, 14%, 75%)',
+      'nav-items-color': 'hsl(223, 64%, 98%)',
+      'accent-clr': 'hsl(26, 100%, 55%)',
+      'accent-light': 'hsl(25, 100%, 94%)'
+
+    },},
   },
   plugins: [],
 }
