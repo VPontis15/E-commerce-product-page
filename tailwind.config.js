cb2ba014b5b130aabbbc1ea67fc01554cbfb1e98
Vassilis Pontis <vpontis15@gmail.com>
2024-02-10 21:08:00 +0200

    add package for tailwind classess rearranging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-      'text-heading-clr': 'hsl(220, 13%, 13%)',
-      'text-clr': 'hsl(220, 14%, 75%)',
-      'text-nav-items-color': 'hsl(223, 64%, 98%)',
-      'text-accent-clr': 'hsl(26, 100%, 55%)',
-      'text-accent-light': 'hsl(25, 100%, 94%)'
-
-    },},
+        'heading-clr': 'hsl(220, 13%, 13%)',
+        main: 'hsl(219, 9%, 45%)',
+        'nav-items-color': 'hsl(220, 14%, 75%)',
+        'accent-clr': 'hsl(26, 100%, 55%)',
+        'accent-light': 'hsl(25, 100%, 94%)',
+      },
+      fontFamily: {
+        'ff-main': ['Kumbh Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
