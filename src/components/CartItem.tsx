e3acd5e888eb76a66cf918d0b5368d7179e82d4c
Vassilis Pontis <vpontis15@gmail.com>
2024-02-11 19:13:21 +0200

    add CartItem jsx to render every cart item

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,48 @@
+import { removeFromCart } from "../state/Cart/CartSlice";
+import { useAppDispatch } from "../state/store";
+import { CartItemType } from "../state/types/actions/cart";
+
+const CartItem: React.FC<{ item: CartItemType }> = ({ item }) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="flex items-center gap-4">
+      <img className="w-12 rounded-lg" src={item.thumbnail} alt="" />
+      <div>
+        <span className="text-sm">{item.name}</span>
+        <div className="flex">
+          <p className="text-sm">
+            $
+            <span>
+              {item.discounted_price ? item.discounted_price : item.price}
+            </span>
+            <span> x</span> {item.quantity} $
+            <span className="  font-bold text-heading-clr">
+              {((item.discounted_price || item.price) * item.quantity).toFixed(
+                2,
+              )}
+            </span>
+          </p>
+        </div>
+      </div>
+      <button onClick={() => dispatch(removeFromCart(item))}>
+        <svg
+          width="14"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <defs>
+            <path
+              d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
+              id="a"
+            />
+          </defs>
+          <use fill="#C3CAD9" fillRule="nonzero" xlinkHref="#a" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default CartItem;
