68209edd6f617015fcbce5a5412d17e326fc8f9d
Vassilis Pontis <vpontis15@gmail.com>
2024-02-11 11:44:22 +0200

    extend theme for font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'heading-clr': 'hsl(220, 13%, 13%)',
-        main: 'hsl(219, 9%, 45%)',
-        'nav-items-color': 'hsl(220, 14%, 75%)',
-        'accent-clr': 'hsl(26, 100%, 55%)',
-        'accent-light': 'hsl(25, 100%, 94%)',
+        "heading-clr": "hsl(220, 13%, 13%)",
+        main: "hsl(219, 9%, 45%)",
+        "nav-items-color": "hsl(220, 14%, 75%)",
+        "accent-clr": "hsl(26, 100%, 55%)",
+        "accent-light": "hsl(25, 100%, 94%)",
       },
       fontFamily: {
-        'ff-main': ['Kumbh Sans', 'sans-serif'],
+        "ff-main": ["Kumbh Sans", "sans-serif"],
+      },
+      fontSize: {
+        "1xs": "0.625rem",
       },
     },
   },
