5377699a51a87aaa75c43b278d36babdcc5a75fb
Vassilis Pontis <vpontis15@gmail.com>
2024-02-11 19:11:20 +0200

    refactor main

--- src/components/features/Homepage/Main.tsx
+++ src/components/features/Homepage/Main.tsx
@@ -1,19 +1,52 @@
 import ProductColumn from "../ProductColumn";
 
+import axios from "axios";
+import { UseQueryResult, useQuery } from "react-query";
+
+import ProductImages from "../../ProductImages";
+import { ProductType } from "../../../state/types/productType";
+
+interface Products {
+  products: ProductType[];
+}
+
+async function getProducts(): Promise<Products> {
+  try {
+    const response = await axios.get<Products>(
+      `http://localhost:3001/sneakers`,
+    );
+    const data = response.data;
+    const updatedData = data.map((product: ProductType) => {
+      if (product.onDiscount) {
+        return {
+          ...product,
+          discounted_price:
+            product.price - product.price * product.discountAmount,
+        };
+      } else return product;
+    });
+
+    return updatedData;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw new Error("Failed to fetch products");
+  }
+}
+
 const Main: React.FC = () => {
+  const queryKey = "sneakers";
+  const queryFn = getProducts;
+
+  const { data, error, isLoading }: UseQueryResult<ProductType[], Error> =
+    useQuery(queryKey, queryFn);
+
+  if (isLoading) return <span>Loading...</span>;
+  if (error) return <span>{error.message}</span>;
+  const currentProduct = data[0];
   return (
     <main className=" grid h-full min-h-[80vh] grid-cols-2 items-center    px-10">
-      <span></span>
-      {/* <section>
-        <h4 className=" text-xs font-semibold  uppercase tracking-wider text-accent-clr">
-          Sneaker Company
-        </h4>
-        <h1 className=" text-3xl font-bold capitalize text-heading-clr">
-          Fall Limitied Edition Sneakers
-        </h1>
-        <p>These low-profile sneakers are</p>
-      </section> */}
-      <ProductColumn />
+      <ProductImages product={currentProduct} />
+      <ProductColumn product={currentProduct} />
     </main>
   );
 };
