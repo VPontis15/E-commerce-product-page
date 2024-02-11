b78f8985566a61949c4346c7e307ae61767af133
Vassilis Pontis <vpontis15@gmail.com>
2024-02-11 19:10:59 +0200

    add close modal on outside click

--- src/components/features/Header/UserDetails.tsx
+++ src/components/features/Header/UserDetails.tsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CartModal from "../../CartModal";
 import { useAppSelector } from "../../../state/hook";
-import { useAppDispatch } from "../../../state/store";
-import { removeFromCart } from "../../../state/Cart/CartSlice";
+
+import CartItem from "../../CartItem";
 
 const UserDetails: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const cartItems = useAppSelector((state) => state.cart.items);
-  const dispatch = useAppDispatch();
+  const CartItems = useAppSelector((state) => state.cart.items);
+  const modalRef = useRef<HTMLDivElement>(null);
   function handleIsOpen() {
     setIsOpen(!isOpen);
   }
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        isOpen &&
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <>
       <div className=" flex items-center gap-8">
@@ -22,9 +41,9 @@ const UserDetails: React.FC = () => {
               fillRule="nonzero"
             />
           </svg>
-          {cartItems && cartItems.length > 0 && (
+          {CartItems && CartItems.length > 0 && (
             <span className="absolute -top-3 left-1 flex h-4 w-4 items-center justify-center rounded-full bg-accent-clr text-xs text-white">
-              {cartItems.length}
+              {CartItems.length}
             </span>
           )}
         </button>
@@ -38,59 +57,23 @@ const UserDetails: React.FC = () => {
       </div>
       {isOpen && (
         <CartModal>
-          <span className="border-b-2 p-4  font-semibold  text-heading-clr">
-            Cart
-          </span>
+          <div
+            className=" absolute right-20 top-20 z-[1000] flex min-h-64 w-full max-w-80 flex-col overflow-y-scroll bg-white  py-6 shadow-2xl"
+            ref={modalRef}
+          >
+            <span className="border-b-2 p-4  font-semibold  text-heading-clr">
+              Cart
+            </span>
 
-          <div className="flex h-full flex-1 flex-col items-center justify-center">
-            {cartItems && cartItems.length > 0 ? (
-              cartItems.map((item) => {
-                return (
-                  <div className="flex items-center gap-4">
-                    <img
-                      className="w-12 rounded-lg"
-                      src={item.thumbnail}
-                      alt=""
-                    />
-                    <div>
-                      <span className="text-sm">{item.name}</span>
-                      <div className="flex">
-                        <p className="text-sm">
-                          $
-                          <span>
-                            {item.discounted_price
-                              ? item.discounted_price
-                              : item.price}
-                          </span>
-                          <span> x</span> {item.quantity} $
-                          <span className="  font-bold text-heading-clr">
-                            {item.final_price.toFixed(2)}
-                          </span>
-                        </p>
-                      </div>
-                    </div>
-                    <button onClick={() => dispatch(removeFromCart(item))}>
-                      <svg
-                        width="14"
-                        height="16"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlnsXlink="http://www.w3.org/1999/xlink"
-                      >
-                        <defs>
-                          <path
-                            d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
-                            id="a"
-                          />
-                        </defs>
-                        <use fill="#C3CAD9" fillRule="nonzero" xlinkHref="#a" />
-                      </svg>
-                    </button>
-                  </div>
-                );
-              })
-            ) : (
-              <span>Your cart is empty</span>
-            )}
+            <div className="flex h-full flex-1 flex-col items-center justify-center">
+              {CartItems && CartItems.length > 0 ? (
+                CartItems.map((item) => {
+                  return <CartItem key={item.id} item={item} />;
+                })
+              ) : (
+                <span>Your cart is empty</span>
+              )}
+            </div>
           </div>
         </CartModal>
       )}
